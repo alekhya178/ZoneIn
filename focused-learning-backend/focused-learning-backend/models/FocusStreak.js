f8c84f0db6fa4271678e0050199432e33d81744c
THANMAHI <thanmahi10@gmail.com>
2026-05-07 23:25:09 +0530

    Cleaned up structure and added all 3 components correctly

--- focused-learning-backend/focused-learning-backend/models/FocusStreak.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const focusStreakSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  currentStreakDays: { type: Number, default: 0 },
-  lastStudyDate: { type: Date, default: null }
-}, { timestamps: true });
-
-module.exports = mongoose.model("FocusStreak", focusStreakSchema);
